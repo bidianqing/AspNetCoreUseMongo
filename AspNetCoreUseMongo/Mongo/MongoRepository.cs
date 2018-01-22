21737a33e9405a9e031a55f3ae12a57b3fd2759b
毕殿卿 <bdq123@outlook.com>
2018-01-22 23:25:11 +0800

    *

--- AspNetCoreUseMongo/Mongo/MongoRepository.cs
+++ AspNetCoreUseMongo/Mongo/MongoRepository.cs
@@ -1,13 +1,18 @@
 ﻿using Microsoft.Extensions.Options;
 using MongoDB.Driver;
 using System;
+using System.Collections.Concurrent;
 
 namespace AspNetCoreUseMongo.Mongo
 {
     public class MongoRepository<T> where T : class, new()
     {
         private readonly MongoOptions _options;
-        private readonly IMongoDatabase _database;
+        private static IMongoClient _mongoClient;
+        private static IMongoDatabase _mongoDatabase;
+        private static object _obj = new object();
+
+        private static readonly ConcurrentDictionary<RuntimeTypeHandle, IMongoCollection<T>> _mongoCollections = new ConcurrentDictionary<RuntimeTypeHandle, IMongoCollection<T>>();
         public MongoRepository(IOptions<MongoOptions> optionsAccessor)
         {
             if (optionsAccessor == null)
@@ -15,12 +20,33 @@ namespace AspNetCoreUseMongo.Mongo
                 throw new ArgumentNullException(nameof(optionsAccessor));
             }
             _options = optionsAccessor.Value;
-            _database = new MongoClient(_options.ConnectionString).GetDatabase(_options.DataBaseName);
+
+            if (_mongoClient == null)
+            {
+                lock (_obj)
+                {
+                    if (_mongoClient == null)
+                    {
+                        _mongoClient = new MongoClient(_options.ConnectionString);
+                        _mongoDatabase = _mongoClient.GetDatabase(_options.DataBaseName);
+                    }
+                }
+            }
+
         }
 
         public void Insert(T document)
         {
-            this._database.GetCollection<T>(typeof(T).Name).InsertOne(document);
+            if (_mongoCollections.TryGetValue(document.GetType().TypeHandle, out IMongoCollection<T> collection))
+            {
+                collection.InsertOne(document);
+            }
+            else
+            {
+                collection = _mongoDatabase.GetCollection<T>(typeof(T).Name);
+                _mongoCollections.TryAdd(document.GetType().TypeHandle, collection);
+                collection.InsertOne(document);
+            }
         }
     }
 }
